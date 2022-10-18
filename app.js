4a1f5668a2927af24a00b7023712a261c435c4c5
Momna Ijaz <114725759+momnaijaz10@users.noreply.github.com>
2022-10-18 08:15:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1;
 var yy = today.getFullYear();
-
+if(dd < 10){
+  dd = "0" + dd;
+}
+if(mm < 10){
+  mm = "0" + mm;
+}
 today = dd + " " + mm + " " + yy;
 console.log(today);
